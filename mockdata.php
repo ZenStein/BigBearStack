7e45f000e05d56cc5f4390d6640bca04d17dd9e4
ZenStein <shaneenterprises@gmail.com>
2015-02-08 23:08:24 -0800

    adding mockdata.php

---
+++ mockdata.php
@@ -0,0 +1,4 @@
+$data[0] = ["name":"chris","id":"3","rating":"best"];
+$data[1] = ["name":"john","id":"3","rating":"bad"];
+$data[2] = ["name":"michael","id":"3","rating":"good"];
+print_r(JSON_encode($data));
